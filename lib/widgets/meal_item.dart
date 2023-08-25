d7a5450742a8b25fb69b23a98b57e5919edf5b45
ambi hidalgo <ambio.pk@gmail.com>
2023-08-25 16:23:40 -0700

    Add displaying Meal data on meals screen

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals/models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealItem extends StatelessWidget {
+  const MealItem({super.key, required this.meal});
+
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      //Corrects children that would normally go outside of boundaries.
+      clipBehavior: Clip.hardEdge,
+      //Adds drop shadow.
+      elevation: 2,
+      child: InkWell(
+        onTap: () {},
+        //Stack allows for the creation of column like display, but is able to display multiple elements, i.e. Image, title etc.
+        child: Stack(
+          children: [
+            //Add smooth image transition
+            //Memory Image stores an image in memory
+            //Network Image is able to fetch an image from the internet
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              //Make sure image is not distorted and fits inside box
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+
+            //Position allows for positioning a widget on top of another widget inside a stack.
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      //Very long text . . .
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
